b49bf78a0a1afa1114dfe2394c9394f76487ab27
Kacper Domański <116498271+KDoman@users.noreply.github.com>
2022-11-02 09:34:42 +0100

    Update

--- app.js
+++ app.js
@@ -39,11 +39,19 @@ let addUserBox = function () {
     cityUser.classList.add('city-user');
     let postcodeUser = document.createElement('div');
     postcodeUser.classList.add('postcode-user');
-    // let selectUser = document.createElement('div');
-    // selectUser.classList.add('.select-user');
+
+    // select column
+    let selectUser = document.createElement('div');
+    selectUser.classList.add('select-user');
+    let selectUserFirstImg = document.createElement('img');
+    selectUserFirstImg.setAttribute('src','cross-mark.png');
+    selectUser.append(selectUserFirstImg);
+    let selectUserSecondImg = document.createElement('img');
+    selectUserSecondImg.setAttribute('src','edit.png');
+    selectUser.append(selectUserSecondImg);
 
     // adding all div's into one div
-    newUserBox.append(nameUser,surnameUser,dateUser,cityUser,postcodeUser)
+    newUserBox.append(nameUser,surnameUser,dateUser,cityUser,postcodeUser,selectUser)
 
     // take inner text in inputs to display them on info div's
     let newUser = new Person(inputName.value,inputSurrname.value,inputDate.value,inputCity.value,inputPostcode.value);
@@ -53,6 +61,56 @@ let addUserBox = function () {
     cityUser.innerText = newUser.city;
     postcodeUser.innerText = newUser.postcode;
 }
+
+
+// ZACZNIJ TUTAJ
+
+
+// let deleteUserInfoPanel = function () {
+//     // IMG DELETE in "SELECT" column (On click)
+//     const deleteQuestionPanel = document.querySelector('#delete-question-panel');
+//     const deleteQuestionPanelYesButton = document.querySelector('#delete-question-panel-button-div-yes');
+//     const deleteQuestionPanelNoButton = document.querySelector('#delete-question-panel-button-div-no');
+//     const deleteQuestionPanelInfoUser = document.querySelector('#delete-question-panel-info-user');
+//     deleteQuestionPanel.append(deleteQuestionPanelInfoUser);
+
+//     selectUserFirstImg.addEventListener('click',() => {
+//         deleteQuestionPanel.style.display = 'block';
+
+//         let userInfoName = document.createElement('p');
+//         deleteQuestionPanelInfoUser.append(userInfoName);
+//         userInfoName.innerText = `Name: ${newUser.name}`;
+
+//         let userInfoSurname = document.createElement('p');
+//         deleteQuestionPanelInfoUser.append(userInfoSurname);
+//         userInfoSurname.innerText = `Surname: ${newUser.surname}`
+
+//         let userInfoDate = document.createElement('p');
+//         deleteQuestionPanelInfoUser.append(userInfoDate);
+//         userInfoDate.innerText = `Date of birth: ${newUser.date}`;
+
+//         let userInfoCity = document.createElement('p');
+//         deleteQuestionPanelInfoUser.append(userInfoCity);
+//         userInfoCity.innerText = `City: ${newUser.city}`;
+
+//         let userInfoPostcode = document.createElement('p');
+//         deleteQuestionPanelInfoUser.append(userInfoPostcode);
+//         userInfoPostcode.innerText = `Postcode: ${newUser.postcode}`;
+
+//         deleteQuestionPanel.append(deleteQuestionPanelInfoUser);
+
+//         deleteQuestionPanelYesButton.addEventListener('click',() => {
+//             newUserBox.remove();
+//             deleteQuestionPanel.style.display = 'none'
+//         })
+//         deleteQuestionPanelNoButton.addEventListener('click',() => {
+//             deleteQuestionPanel.style.display = 'none'
+//         })
+//     })
+// }
+
+
+
 
 // Plus sign on click
 plusSign.addEventListener('click',() => {
